50b7adcde9116c72b62bc9b2e3f2571b76ddc272
Ernest Manukyan <erik.manukyan2010@yandex.ru>
2024-05-28 10:30:23 +0400

    feat: adding under construction component

---
+++ components/UnderConstruction.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="font-mono flex flex-col gap-7 items-center">
+    <h1 class="font-500 text-10 text-primary">
+      Новый дизайн скоро подъедет
+    </h1>
+
+    <h3 class="text-6 text-secondary">
+      А пока мы активно трудимся над тем, чтобы сайт выглядел круто и хорошо работал :)
+    </h3>
+  </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="h-dvh grid place-items-center relative overflow-y-hidden">
+    <UnderConstruction class="z-2" />
+
+    <div class="pos-absolute pos-bottom-0 pos-left-0 pos-right-0 z-1">
+      <img class="w-full" src="/public/flowers.png">
+    </div>
+  </div>
+</template>
