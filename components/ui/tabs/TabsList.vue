ea300dc6a0fb26ea362f7bcd4416adb6711b3ac6
Ernest Manukyan <erik.manukyan2010@yandex.ru>
2024-08-01 00:27:34 +0400

    style: update tabs styles

--- components/ui/tabs/TabsList.vue
+++ components/ui/tabs/TabsList.vue
@@ -16,7 +16,7 @@ const delegatedProps = computed(() => {
   <TabsList
     v-bind="delegatedProps"
     :class="cn(
-      'inline-flex h-10 items-center justify-center rounded-md bg-blue-50 p-1',
+      'inline-flex items-center justify-center rounded-md bg-blue-50 p-1',
       props.class,
     )"
   >
