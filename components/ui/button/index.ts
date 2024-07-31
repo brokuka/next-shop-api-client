fb1fc3282ddd139e0ec1031e85d1f3176a81e81a
Ernest Manukyan <erik.manukyan2010@yandex.ru>
2024-07-31 12:56:22 +0400

    chore: update button link variant

--- components/ui/button/index.ts
+++ components/ui/button/index.ts
@@ -3,12 +3,12 @@ import { type VariantProps, cva } from 'class-variance-authority'
 export { default as Button } from './Button.vue'
 
 export const buttonVariants = cva(
-  'inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm ring-offset-background transition-colors focus-visible:(outline-none ring-2 ring-ring ring-offset-2) disabled:(opacity-50 cursor-not-allowed) min-w-[204px]',
+  'inline-flex items-center justify-center whitespace-nowrap rounded-md ring-offset-background transition-colors focus-visible:(outline-none ring-2 ring-ring ring-offset-2) disabled:(opacity-50 cursor-not-allowed)',
   {
     variants: {
       variant: {
-        default: 'bg-blue-600 text-base-100 hover:bg-blue-700 font-medium active:bg-blue-800 focus-visible:bg-blue-700 disabled:(bg-neutral-100 text-neutral-400)',
-        link: 'hover:underline underline-offset-3 text-blue-600',
+        default: 'bg-blue-600 text-base-100 hover:bg-blue-700 text-sm font-medium min-w-[204px] active:bg-blue-800 focus-visible:bg-blue-700 disabled:(bg-neutral-100 text-neutral-400)',
+        link: 'hover:underline underline-offset-3 text-blue-600 text-xs',
       },
       size: {
         default: 'h-10 px-4 py-2',
